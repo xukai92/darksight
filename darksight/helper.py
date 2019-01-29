d89421d3f9fe6f656166a3b60a17bcfd5e151934
Kai Xu <xukai921110@gmail.com>
2019-01-29 01:42:47 +0000

    rfc code

--- darksight/helper.py
+++ darksight/helper.py
@@ -2,12 +2,14 @@ import torch
 
 
 def sym_kl_div(P, Q):
-    div_total = 0.5 * torch.sum(P * (torch.log(P) - torch.log(Q))) + \
-                0.5 * torch.sum(Q * (torch.log(Q) - torch.log(P)))
-    return div_total / P.size(0)
+    return _sym_kl_div(torch.log(P) - torch.log(Q), P, Q)
 
 
 def sym_kl_div_with_log_input(log_P, log_Q):
-    div_total = 0.5 * torch.sum(torch.exp(log_P) * (log_P - log_Q)) + \
-                0.5 * torch.sum(torch.exp(log_Q) * (log_Q - log_P))
-    return div_total / log_P.size(0)
+    return _sym_kl_div(log_P - log_Q, torch.exp(log_P), torch.exp(log_Q))
+  
+  
+def _sym_kl_div(logdiff, P, Q):
+    div_total = 0.5 * torch.sum(P * ( logdiff)) + \
+                0.5 * torch.sum(Q * (-logdiff))
+    return div_total / logdiff.size(0)
